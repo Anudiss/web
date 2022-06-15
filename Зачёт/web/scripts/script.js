60cbcf8272775ad16d59bce5dd6724a52564f8e4
Anudiss <liobara31@gmail.com>
2022-06-16 00:11:44 +0300

    и

--- Зачёт/web/scripts/script.js
+++ Зачёт/web/scripts/script.js
@@ -1,3 +1,43 @@
+
 $(document).ready(function () {
+    const scrollValue = $('.inner__reviews').width();
+    const maxScroll = $('.inner__reviews').width();
+    const scrollTime = 300;
+    let scroll = 0;
+
+    $('.arrow.left').bind('click', function(e){
+        ScrollUp();
+    });
+
+    $('.arrow.right').bind('click', function(e){
+        ScrollDown();
+    });
+
+    function ScrollUp() {
+        scroll -= scrollValue;
+        scroll = Clamp(0, maxScroll, scroll);
+        $('.inner__reviews').animate({scrollLeft:scroll}, scrollTime, 'linear');
+    }
+    
+    function ScrollDown() {
+        scroll += scrollValue;
+        scroll = Clamp(0, maxScroll, scroll);
+        $('.inner__reviews').animate({scrollLeft:scroll}, scrollTime, 'linear');
+    }
     
 });
+
+function Clamp(min, max, value)
+{
+    return Max(min, Min(max, value));
+}
+
+function Min(v1, v2)
+{
+    return v1 > v2 ? v2 : v1;
+}
+
+function Max(v1, v2)
+{
+    return v1 < v2 ? v2 : v1;
+}
